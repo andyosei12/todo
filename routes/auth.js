241638575ce7d3b4c6121df089c5a1dbd6e77279
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-10-10 15:59:46 +0000

    add user login

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { registerUser, loginUser } = require('../controllers/auth');
 const validateUserCreation = require('../middleware/auth/validateUserCreation');
-const { registerUser } = require('../controllers/auth');
+const validateLogin = require('../middleware/auth/validateLogin');
 
 const router = express.Router();
 
 router.post('/signup', validateUserCreation, registerUser);
+router.post('/login', validateLogin, loginUser);
 
 module.exports = router;
