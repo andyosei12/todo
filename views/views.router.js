2bdf763a9e66508f738db564a70cda95fbb545c4
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-10-14 05:13:40 +0000

    feat-add auth and protect routes (#6)

--- views/views.router.js
+++ views/views.router.js
@@ -1,17 +1,121 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const userServices = require('../users/users.services');
+const taskServices = require('../tasks/tasks.services');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index');
-});
+router.use(cookieParser());
 
 router.get('/user/login', (req, res) => {
-  res.render('login');
+  res.render('login', { error: null });
+});
+
+router.post('/user/login', async (req, res) => {
+  const response = await userServices.loginUser(req.body);
+  if (response.error) {
+    return res.render('login', { error: response.message });
+  } else {
+    res.cookie('jwt', response.data.token);
+    res.redirect('/');
+  }
 });
 
 router.get('/user/signup', (req, res) => {
-  res.render('signup');
+  res.render('signup', { error: null });
+});
+
+router.post('/user/signup', async (req, res) => {
+  const response = await userServices.registerUser(req.body);
+  if (response.error) {
+    return res.render('signup', { error: response.message });
+  } else {
+    res.cookie('jwt', response.data.token);
+    res.redirect('/');
+  }
+});
+
+router.use(async (req, res, next) => {
+  if (req.cookies) {
+    try {
+      const decodedValue = await jwt.verify(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
+
+      res.locals.user = decodedValue;
+      next();
+    } catch (error) {
+      res.redirect('/user/login');
+    }
+  } else {
+    res.redirect('/user/login');
+  }
+});
+
+router.get('/', async (req, res) => {
+  const userId = res.locals.user.id;
+  const response = await taskServices.getTasks(userId);
+  res.render('index', {
+    user: res.locals.user,
+    error: null,
+    tasks: response.tasks,
+  });
+});
+
+router.get('/logout', (req, res) => {
+  res.clearCookie('jwt');
+  res.redirect('/user/login');
+});
+
+// Add a task
+router.post('/task', async (req, res) => {
+  const response = await taskServices.addTask(req.body);
+  if (response.error) {
+    return res.redirect('/', { error: response.message });
+  } else {
+    res.redirect('/');
+  }
+});
+
+// Get tasks
+router.get('/tasks', async (req, res) => {
+  const userId = res.locals.user.id;
+  const { status } = req.query;
+  const response = await taskServices.getTasks(userId, status);
+  if (response.error) {
+    return res.redirect('/', { error: response.message });
+  } else {
+    res.render('index', {
+      user: res.locals.user,
+      error: null,
+      tasks: response.tasks,
+    });
+  }
+});
+
+// Update task
+router.post('/task/:taskId', async (req, res) => {
+  const { taskId } = req.params;
+  const response = await taskServices.updateTask(taskId);
+  if (response.error) {
+    return res.redirect('/', { error: response.message });
+  } else {
+    res.redirect('/');
+  }
+});
+
+// Delete task
+router.post('/task/:taskId/remove', async (req, res) => {
+  const { taskId } = req.params;
+  console.log(taskId);
+  const response = await taskServices.deleteTask(taskId);
+  if (response.error) {
+    return res.redirect('/', { error: response.message });
+  } else {
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
