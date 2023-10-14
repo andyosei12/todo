6c8bbb30ee9f4a8fdbc245db06e9b9e063cbdc5f
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-10-14 06:21:21 +0000

    add authHeader partial

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const path = require('path');
 const dotenv = require('dotenv');
 const db = require('./db');
 const viewsRouter = require('./views/views.router');
-const authRouter = require('./routes/auth');
-const taskRouter = require('./routes/task');
-const verifyUser = require('./middleware/auth/verifyUser');
 
 dotenv.config();
 const app = express();
@@ -20,8 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.use('/', viewsRouter);
 
-app.use('/api/v1/auth/', authRouter);
-app.use('/api/v1/tasks', taskRouter);
+// app.use('/api/v1/auth/', authRouter);
+// app.use('/api/v1/tasks', taskRouter);
 
 app.listen(port, () => {
   console.log('server started on port', port);
