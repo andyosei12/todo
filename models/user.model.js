db4c723066467a04429a8f8c4e833eb45405c055
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-10-18 06:22:49 +0000

    fix user and task models (#13)

--- models/user.model.js
+++ models/user.model.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
-  user_name: { type: String, unique: true },
+  user_name: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   created_at: { type: Date, default: new Date() },
 });
